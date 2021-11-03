f9977fba807820dac05abcdc699697d4719abfca
Elchanan Vol <elchanan.vol@gmail.com>
2021-11-03 17:24:36 +0200

    end of ex2

--- ex2/src/Board.java
+++ ex2/src/Board.java
@@ -3,65 +3,93 @@ import java.util.Arrays;
 public class Board {
     public static final int SIZE = 4;
     public static final int WIN_STREAK = 3;
-    private Mark[][] board= new Mark[SIZE][SIZE];
+    private Mark[][] board = new Mark[SIZE][SIZE];
     private boolean game_ended = false;
     private Mark winner = Mark.BLANK;
+    int counter_hold_places = 0;
+
+    /**
+     * constructor. fill the board with empty sign.
+     */
     Board() {
-        for(int i=0;i<SIZE;i++) {
+        for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 board[i][j] = Mark.BLANK;
             }
         }
     }
-    public Mark getMark(int i, int j){
-        if (i>SIZE || i<1 || j>SIZE || j<1)
-        {
+
+    /**
+     * getter method to get board[i,j]
+     *
+     * @param i i coordinate
+     * @param j j coordinate
+     * @return the mark in board[i,j] if it filled and i,j legal, else mark.blank will return
+     */
+
+    public Mark getMark(int i, int j) {
+        if (i >= SIZE || i < 0 || j >= SIZE || j < 0) {
             return Mark.BLANK;
         }
         return board[i][j];
     }
-    boolean putMark(Mark mark,int i, int j){
-        if ((i<=SIZE && i>0) || (j<=SIZE && j>0))
-        {
-            if (board[i][j] ==Mark.BLANK){
-                board[i][j] =mark;
+
+    /**
+     * if there is empty place at board[i,j] for mark, this method will put it in the matrix
+     *
+     * @param mark mark of player to put
+     * @param i    i coordinate
+     * @param j    j coordinate
+     * @return false if board[i,j] out of range or if cell is filled. true upon successes
+     */
+    public boolean putMark(Mark mark, int i, int j) {
+        if (i < SIZE && i >= 0 && j < SIZE && j >= 0) {
+            if (board[i][j] == Mark.BLANK) {
+                board[i][j] = mark;
+                counter_hold_places++;
                 return true;
             }
         }
         return false;
     }
-    public boolean gameEnded(){
-        if(game_ended){
+
+    /**
+     * check if the game and. go through all cells of the mat and check if the game end.
+     * assumption: mark.blank is empty cell and the marks of players is {Mark.O, Mark.X}
+     *
+     * @return true if there is winner or that all cells are full.
+     */
+    public boolean gameEnded() {
+        if (game_ended) {
             return true;
         }
-        Mark[] temp ={Mark.O,Mark.X};
-        for (Mark mark : temp){
-            for (int row=0;row<SIZE;row++)
-            {
-                for (int col=0;col<SIZE;col++)
-                {
-                    int counter_row =0;
-                    int counter_col =0;
-                    int counter_diag =0;
-                    for(int k=0;k<WIN_STREAK;k++){
-                        if (getMark(row+k,col)== mark)
-                        {
+        Mark[] temp = {Mark.O, Mark.X};
+        for (Mark mark : temp) {
+            for (int row = 0; row < SIZE; row++) {
+                for (int col = 0; col < SIZE; col++) {
+                    int counter_row = 0;
+                    int counter_col = 0;
+                    int counter_diag = 0;
+                    int counter_sec_diag = 0;
+                    for (int k = 0; k < WIN_STREAK; k++) {
+                        if (getMark(row + k, col) == mark) {
                             counter_row++;
                         }
-                        if (getMark(row,col+k)== mark)
-                        {
+                        if (getMark(row, col + k) == mark) {
                             counter_col++;
                         }
-                        if (getMark(row+k,col+k)== mark)
-                        {
+                        if (getMark(row + k, col + k) == mark) {
                             counter_diag++;
+                        }
+                        if (getMark(row + k, col - k) == mark) {
+                            counter_sec_diag++;
                         }
 
                     }
-                    if (counter_row ==WIN_STREAK || counter_col ==WIN_STREAK||counter_diag ==WIN_STREAK)
-                    {
-                       winner = mark;
-                       return true;
+                    if (counter_row == WIN_STREAK || counter_col == WIN_STREAK ||
+                            counter_diag == WIN_STREAK || counter_sec_diag == WIN_STREAK) {
+                        winner = mark;
+                        return true;
                     }
 
                 }
@@ -69,19 +97,17 @@ public class Board {
             }
 
         }
-        for (int row=0;row<SIZE;row++) {
-            for (int col = 0; col < SIZE; col++) {
-                if (board[row][col] == Mark.BLANK){
-                    return false;
-                }
-
-            }
-        }
-
-        return true;
+        return counter_hold_places >= SIZE * SIZE;
     }
 
+    /**
+     * this method will return the winner in the game.
+     *
+     * @return mark of the winner
+     */
+
     public Mark getWinner() {
+        gameEnded();
 
         return winner;
     }
--- ex2/src/Game.java
+++ ex2/src/Game.java
@@ -1,8 +1,62 @@
+//import javax.swing.Renderer;
+
 public class Game {
-    public static void main(String[] args) {
-        Renderer printer = new Renderer();
-        Board board = new Board();
-        board.putMark(Mark.X,2,2);
-        printer.renderBoard(board);
+    private Player playerX;
+    private Player playerO;
+    private Renderer renderer;
+
+    public Game(Player playerX, Player playerO, Renderer renderer) {
+        this.playerX = playerX;
+        this.playerO = playerO;
+        this.renderer = renderer;
+
     }
+
+    /**
+     * main method to run the game. it play the game until its end.
+     *
+     * @return the winner of the game.
+     */
+    public Mark run() {
+        Board board = new Board();
+        int i = 0;
+        Player[] players = {playerX, playerO};
+        Mark[] marks = {Mark.X, Mark.O};
+        while (!board.gameEnded()) {
+            players[i].playTurn(board, marks[i]);
+            renderer.renderBoard(board);
+            i = 1 - i;
+        }
+        return board.getWinner();
+
+    }
+
+
+    public static void main(String[] args) {
+        Renderer r = new Renderer();
+        Player pa = new Player();
+        Player pb = new Player();
+        Board b = new Board();
+
+
+        for (int i = 0; i < 3; i++) {
+            b.putMark(Mark.X, 3, 1 + i);
+        }
+        Mark winner = b.getWinner();
+        r.renderBoard(b);
+
+//        Game game = new Game(pa,pb,r);
+//        Mark winner = game.run();
+
+        if (winner == Mark.X) {
+            System.out.print("X win");
+        }
+        if (winner == Mark.O) {
+            System.out.print("O win");
+        }
+
+
+    }
+
+
 }
