913d4eec2009a5b02f7087d62b1098e032655c08
Elchanan Vol <elchanan.vol@gmail.com>
2021-11-22 01:46:00 +0200

    create

--- ex3/src/gameobjects/GraphicLifeCounter.java
+++ ex3/src/gameobjects/GraphicLifeCounter.java
@@ -1,41 +1,72 @@
-//package src.gameobjects;
-//
-//import danogl.GameObject;
-//import danogl.collisions.GameObjectCollection;
-//import danogl.collisions.Layer;
-//import danogl.gui.rendering.Renderable;
-//import danogl.util.Counter;
-//import danogl.util.Vector2;
-//
-///**
-// * Display a graphic object on the game window showing a numeric count of lives left.
-// */
-//public class GraphicLifeCounter extends danogl.GameObject {
-//
-//    /**
-//     *
-//     * @param widgetTopLeftCorner top left corner of left most life widgets. Other widgets will be displayed
-//     *                           to its right, aligned in hight.
-//     * @param widgetDimensions dimensions of widgets to be displayed.
-//     * @param livesCounter  global lives counter of game.
-//     * @param widgetRenderable image to use for widgets.
-//     * @param gameObjectsCollection global game object collection managed by game manager.
-//     * @param numOfLives global setting of number of lives a player will have in a game.
-//     */
-//    public GraphicLifeCounter(danogl.util.Vector2 widgetTopLeftCorner,
-//                               danogl.util.Vector2 widgetDimensions,
-//                               danogl.util.Counter livesCounter,
-//                               danogl.gui.rendering.Renderable widgetRenderable,
-//                               danogl.collisions.GameObjectCollection gameObjectsCollection,
-//                               int numOfLives) {
-//
-//    }
-//
-//    /**
-//     * Overrides update in class danogl.GameObject
-//     */
-//    @Override
-//    public void update(float deltaTime){
-//
-//    }
-//}
+package src.gameobjects;
+
+import danogl.GameObject;
+import danogl.collisions.GameObjectCollection;
+import danogl.collisions.Layer;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Counter;
+import danogl.util.Vector2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Display a graphic object on the game window showing a numeric count of lives left.
+ */
+public class GraphicLifeCounter extends danogl.GameObject {
+
+    private final GameObjectCollection gameObjectCollection;
+    private final Counter counter;
+    private int numOfLives;
+    private List<GameObject> widgets;
+
+    /**
+     *
+     * @param widgetTopLeftCorner top left corner of left most life widgets. Other widgets will be displayed
+     *                           to its right, aligned in hight.
+     * @param widgetDimensions dimensions of widgets to be displayed.
+     * @param livesCounter  global lives counter of game.
+     * @param widgetRenderable image to use for widgets.
+     * @param gameObjectsCollection global game object collection managed by game manager.
+     * @param numOfLives global setting of number of lives a player will have in a game.
+     */
+    public GraphicLifeCounter(danogl.util.Vector2 widgetTopLeftCorner,
+                               danogl.util.Vector2 widgetDimensions,
+                               danogl.util.Counter livesCounter,
+                               danogl.gui.rendering.Renderable widgetRenderable,
+                               danogl.collisions.GameObjectCollection gameObjectsCollection,
+                               int numOfLives) {
+        super(widgetTopLeftCorner, widgetDimensions, widgetRenderable);
+        this.gameObjectCollection = gameObjectsCollection;
+        widgets = new ArrayList<>();
+        Vector2 location =widgetTopLeftCorner;
+        for(int i =0;i<livesCounter.value()-1;i++){
+            location =location.add(new Vector2(widgetDimensions.x(),0));
+            GameObject live = new GameObject(location, widgetDimensions, widgetRenderable);
+            widgets.add(live);
+            gameObjectsCollection.addGameObject(live, Layer.BACKGROUND);
+        }
+        gameObjectsCollection.addGameObject(this, Layer.BACKGROUND);
+
+        counter = livesCounter;
+        this.numOfLives = numOfLives-1;
+
+
+
+
+    }
+
+    /**
+     * Overrides update in class danogl.GameObject
+     */
+    @Override
+    public void update(float deltaTime){
+
+        if (counter.value()<numOfLives+1)
+        {
+            gameObjectCollection.removeGameObject(widgets.get(numOfLives-1),Layer.BACKGROUND);
+            widgets.remove(numOfLives-1);
+            numOfLives--;
+        }
+    }
+}
--- ex3/src/gameobjects/NumericLifeCounter.java
+++ ex3/src/gameobjects/NumericLifeCounter.java
@@ -1,38 +1,55 @@
-//package src.gameobjects;
-//
-//import danogl.GameObject;
-//import danogl.collisions.GameObjectCollection;
-//import danogl.collisions.Layer;
-//import danogl.gui.rendering.*;
-//import danogl.util.Counter;
-//import danogl.util.Vector2;
-//import java.awt.*;
-//
-///**
-// * handles the numeric lives of the game.
-// */
-//public class NumericLifeCounter extends danogl.GameObject {
-//
-//    /**
-//     * Constructor.
-//     *
-//     * @param livesCounter         global lives counter of game.
-//     * @param topLeftCorner        top left corner of renderable
-//     * @param dimensions           dimensions of renderable
-//     * @param gameObjectCollection global game object collection
-//     */
-//    public NumericLifeCounter(danogl.util.Counter livesCounter,
-//                              danogl.util.Vector2 topLeftCorner,
-//                              danogl.util.Vector2 dimensions,
-//                              danogl.collisions.GameObjectCollection gameObjectCollection) {
-//
-//    }
-//
-//    /**
-//     * Overrides update in class danogl.GameObject
-//     */
-//    @Override
-//    public void update(float deltaTime) {
-//
-//    }
-//}
+package src.gameobjects;
+
+import danogl.GameObject;
+import danogl.collisions.GameObjectCollection;
+import danogl.collisions.Layer;
+import danogl.gui.rendering.TextRenderable;
+import danogl.util.Counter;
+import danogl.util.Vector2;
+import java.awt.*;
+
+/**
+ * handles the numeric lives of the game.
+ */
+public class NumericLifeCounter extends danogl.GameObject {
+    private final Counter livesCounter;
+    private int numOfLivesChanged;
+
+    /**
+     * Constructor.
+     *
+     * @param livesCounter         global lives counter of game.
+     * @param topLeftCorner        top left corner of renderable
+     * @param dimensions           dimensions of renderable
+     * @param gameObjectCollection global game object collection
+     */
+    public NumericLifeCounter(danogl.util.Counter livesCounter,
+                              danogl.util.Vector2 topLeftCorner,
+                              danogl.util.Vector2 dimensions,
+                              danogl.collisions.GameObjectCollection gameObjectCollection) {
+
+        super(topLeftCorner,dimensions,null);
+        gameObjectCollection.addGameObject(this,Layer.BACKGROUND);
+        this.livesCounter = livesCounter;
+        this.numOfLivesChanged =livesCounter.value()+1;
+
+
+
+    }
+
+    /**
+     * Overrides update in class danogl.GameObject
+     */
+    @Override
+    public void update(float deltaTime) {
+        if (livesCounter.value()<numOfLivesChanged)
+        {
+            TextRenderable t = new TextRenderable("");
+            t.setString(Integer.toString(livesCounter.value()));
+            this.renderer().setRenderable(t);
+            numOfLivesChanged--;
+        }
+    }
+
+
+}
